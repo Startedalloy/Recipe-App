a99229c5426c5b456e534133105b708dc1779cd8
Startedalloy <kamranzaheer345@gmail.com>
2025-12-31 19:59:26 +0500

    Fix Some Minors Error

--- app/src/main/java/com/example/recipeapp/RecipeView.kt
+++ app/src/main/java/com/example/recipeapp/RecipeView.kt
@@ -7,10 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
@@ -29,8 +27,8 @@ fun RecipeView() {
     val viewState = recipeModel.categoriesState
 
     if (viewState.value.loading) {
-        //Loading Screen
-    } else if (viewState.value.loading == false) {
+        CircularProgressIndicator()
+    } else if (!viewState.value.loading) {
         MainScreen(viewState.value.list)
     } else {
         Text("Unknown Error")
@@ -40,7 +38,9 @@ fun RecipeView() {
 
 @Composable
 fun MainScreen(categories: List<Category>) {
-    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize().padding(5.dp)) {
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier
+        .fillMaxSize()
+        .padding(5.dp)) {
         items(categories) { category ->
             CategoryScreen(category = category)
 
@@ -51,20 +51,22 @@ fun MainScreen(categories: List<Category>) {
 
 @Composable
 fun CategoryScreen(category: Category) {
-        Column(Modifier.fillMaxWidth()) {
-            Image(
-                painter = rememberAsyncImagePainter(category.strCategoryThumb),
-                contentDescription = "Picture Of Food",
-                modifier = Modifier
-                    .fillMaxSize()
-                    .aspectRatio(1f)
-            )
-            Text(
-                text = category.strCategory,
-                fontSize = (24.sp),
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
-                modifier = Modifier.padding(top = 5.dp).align(Alignment.CenterHorizontally)
-            )
-        }
+    Column(Modifier.fillMaxWidth()) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = "Picture Of Food",
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+        )
+        Text(
+            text = category.strCategory,
+            fontSize = (24.sp),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(top = 5.dp)
+                .align(Alignment.CenterHorizontally)
+        )
     }
+}
